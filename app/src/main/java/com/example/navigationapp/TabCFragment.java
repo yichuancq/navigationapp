b030c5fc2067685828efdc99dd08640320e861de
yichuancq <1012027293@qq.com>
2019-03-10 01:32:45 +0800

    add adapter侧滑删除

--- app/src/main/java/com/example/navigationapp/TabCFragment.java
+++ app/src/main/java/com/example/navigationapp/TabCFragment.java
@@ -3,12 +3,19 @@ package com.example.navigationapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TabHost;
+import android.widget.AbsListView;
+import android.widget.ListView;
+
+import com.daimajia.swipe.util.Attributes;
+import com.example.navigationapp.adapter.SwipeAdapter;
 
 public class TabCFragment extends Fragment {
+    private ListView mListView;
+    private SwipeAdapter mAdapter;
 
     public static TabCFragment newInstance(String text) {
         TabCFragment fragmentCommon = new TabCFragment();
@@ -22,19 +29,60 @@ public class TabCFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //不同的Activity对应不同的布局
-        View view = inflater.inflate(R.layout.table_host_layout, container, false);
-
-        TabHost tab = (TabHost) view.findViewById(android.R.id.tabhost);
-
-        //初始化TabHost容器
-        tab.setup();
-        //在TabHost创建标签，然后设置：标题／图标／标签页布局
-        tab.addTab(tab.newTabSpec("tab1").setIndicator("本地音乐", null).setContent(R.id.tab1));
-        tab.addTab(tab.newTabSpec("tab2").setIndicator("网络音乐", null).setContent(R.id.tab2));
-        tab.addTab(tab.newTabSpec("tab3").setIndicator("我的收藏", null).setContent(R.id.tab3));
-        tab.addTab(tab.newTabSpec("tab4").setIndicator("我的购物车", null).setContent(R.id.tab4));
-        tab.addTab(tab.newTabSpec("tab6").setIndicator("我的积分", null).setContent(R.id.tab5));
+        View view = inflater.inflate(R.layout.fragment_tab3, container, false);
+        //listview2
+        mListView = (ListView) view.findViewById(R.id.listview2);
+        setAdapter();
         return view;
+    }
+
+    private void setAdapter() {
+        mAdapter = new SwipeAdapter(getActivity().getApplicationContext());
+        mListView.setAdapter(mAdapter);
+        mAdapter.setMode(Attributes.Mode.Single);
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                ((SwipeLayout) (mListView.getChildAt(position - mListView.getFirstVisiblePosition()))).open(true);
+//            }
+//        });
+//        mListView.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                Log.i("ListView", "OnTouch");
+//                return false;
+//            }
+//        });
+//        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//                //Toast.makeText(mContext, "OnItemLongClickListener", Toast.LENGTH_SHORT).show();
+//                return true;
+//            }
+//        });
+        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                Log.i("ListView", "onScrollStateChanged");
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            }
+        });
+
+//        mListView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Log.i("ListView", "onItemSelected:" + position);
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//                Log.i("ListView", "onNothingSelected:");
+//            }
+//        });
     }
 }
 
