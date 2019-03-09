6899f0bd9a4cca5760303f39a3a6faaa91fa0fce
yichuancq <1012027293@qq.com>
2019-03-09 15:24:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/navigationapp/SubOneActivity.java
@@ -0,0 +1,25 @@
+package com.example.navigationapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SubOneActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //不同的Activity对应不同的布局
+        View view = inflater.inflate(R.layout.activity_sub_one_layout, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // todo  布局
+    }
+}
